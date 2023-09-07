99379d73c6cfff4e3cc7681952900bc5061eaf0e
SabrinaPutriAulia <21081010048@student.upnjatim.ac.id>
2023-09-07 20:54:40 +0700

    first_commit

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ for (let x in arr) {
 for (let y of arr) {
   console.log(y);
 }
+
+let plat = "kuNing";
+let jenisKendaraan = "Mobil";
+let cc = 1500;
+
+const jenisBBM =
+  plat.toLowerCase() === "kuning" || jenisKendaraan.toLLowerCase() === "motor"
+    ? "BBM Subsidi"
+    : jenisKendaraan === "Mobil" && cc < 1500
+    ? "PERTAMAX"
+    : jenisKendaraan === "Mobil" && cc >= 1500
+    ? "Pertamax Turbo"
+    : "Tidak Terdefinisi";
+
+console.log("Jenis BBM : " + jenisBBM);
