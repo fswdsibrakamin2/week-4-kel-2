1b98dec1f0cbba0b6c540eb43a3f824233abe203
Mukhamad Sa'id Aqil <142391294+Qilaqil14@users.noreply.github.com>
2023-09-07 20:59:05 +0700

    dari aqil

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # week-4-kel-2
 
 Halo, nama saya Fauzan!
+Halo, nama saya Aqil!
 
 Halo, nama saya Sabrina!
--- index.js
+++ index.js
@@ -9,6 +9,20 @@ for (let y of arr) {
   console.log(y);
 }
 
+<<<<<<< HEAD
+// codingan suhu air Aqil
+
+let SUHU = -1; //isi sesuai suhu
+let grade =
+  SUHU >= -100 && SUHU <= 0
+    ? "BEKU"
+    : SUHU >= 1 && SUHU <= 100
+    ? "CAIR"
+    : SUHU >= 101 && SUHU <= 500
+    ? "UAP"
+    : "TIDAK TERDEFINISI";
+console.log(grade);
+=======
 let plat = "kuNing";
 let jenisKendaraan = "Mobil";
 let cc = 1500;
@@ -23,3 +37,5 @@ const jenisBBM =
     : "Tidak Terdefinisi";
 
 console.log("Jenis BBM : " + jenisBBM);
+
+>>>>>>> 99379d73c6cfff4e3cc7681952900bc5061eaf0e
