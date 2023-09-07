04de58c49ffd9d40f733517c589886d98fbdd1c2
Ap-pamungkas <agustinusputra94@gmail.com>
2023-09-07 20:56:32 +0700

    agustinus putra pamungkas

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # week-4-kel-2
 
 Halo, nama saya Fauzan!
+
+
+
+
+
+
+
+Halo, nama saya Putra
--- index.js
+++ index.js
@@ -8,3 +8,33 @@ for (let x in arr) {
 for (let y of arr) {
   console.log(y);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let suhu = prompt("input suhu");
+
+const kondisiAir = suhu >= -100 && suhu <= 0 
+? " beku" : suhu >=1 && suhu <= 100 
+? " cair": suhu >=101 && suhu <=500
+? " uap" :" tidak terdefinisi";
+
+console.log(`Air Dalam Keadaan : ${kondisiAir}`);
+
