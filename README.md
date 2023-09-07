f3e12d573f25f4f6583d0dfb3872809dd7794a8d
Gilang Fatahilah Akbar <gilangf010@gmail.com>
2023-09-07 20:55:19 +0700

    Update Gilang

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # week-4-kel-2
 
 Halo, nama saya Fauzan!
+
+Halo, nama saya Gilang!
--- index.js
+++ index.js
@@ -8,3 +8,50 @@ for (let x in arr) {
 for (let y of arr) {
   console.log(y);
 }
+
+// Gilang Fatahilah
+
+(async () => {
+  // Membuat Prompt Input
+  const { value: degrees } = await Swal.fire({
+    title: "Input Degrees",
+    input: "number",
+    inputLabel: "Enter Degrees",
+    inputPlaceholder: "Enter the number of degrees",
+  });
+
+  if (degrees) {
+    const degree = degrees;
+
+    // Membuat Logika Kondisi Sesuai Dengan Soal
+    const condition =
+      degree <= 0 && degree >= -100
+        ? "Beku"
+        : degree >= 1 && degree <= 100
+        ? "Cair"
+        : degree >= 101 && degree <= 500
+        ? "Uap"
+        : "Tidak Terdefinisi";
+
+    // Menampilkan hasil dari input dan kondisi
+    await Swal.fire({
+      title: "Result :",
+      text: `Suhu: ${degree} Derajat Merupakan Kondisi "${condition}"`,
+      icon: "success",
+      confirmButtonText: "Case 2",
+    }).then((result) => {
+      // Mengalihkan Halaman ke case 2 Apabila ditekan
+      if (result) {
+        window.location.href = "case2.html";
+      }
+    });
+  } else {
+    // Jika user memberi inputan yang salah
+    Swal.fire({
+      title: "Undefined",
+      text: "Please Enter the Correct Input",
+      icon: "error",
+      confirmButtonText: "Next",
+    });
+  }
+})();
